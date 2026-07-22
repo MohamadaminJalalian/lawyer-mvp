f2d12a0b25e5350763778b5930f5b70dd955a532
King Tarokh <kingtarokh@gmail.com>
2026-07-22 23:37:13 +0330

    Update BaseEntity.cs

--- backend/Backend2api/src/CaseManagement.Domain/Common/BaseEntity.cs
+++ backend/Backend2api/src/CaseManagement.Domain/Common/BaseEntity.cs
@@ -1,1 +1,20 @@
+// CaseManagement.Domain/Common/BaseEntity.cs
+// Authors: Tarokh Torabi & Mohammad Amin Jalalian
+
+namespace CaseManagement.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+
+    public DateTime CreatedAtUtc { get; set; }
+
+    public Guid CreatedByUserId { get; set; }
+
+    public DateTime? UpdatedAtUtc { get; set; }
+
+    public Guid? UpdatedByUserId { get; set; }
+
+    public bool IsArchived { get; set; }
+}
 
